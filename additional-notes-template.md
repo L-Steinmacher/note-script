7c2df1336fb5db5784437f09965002897e1e9737
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-12-07 11:52:43 -0800

    added success message

--- additional-notes-template.md
+++ additional-notes-template.md
@@ -1,11 +1,11 @@
 **Summary**
--
+- In this session...
 
 **Tools / Concepts / Notable Files**
 -
 
 **Questions for Clarification**
-- 
+-
 
 **Additional Content Ideas**
 -
--- note-script.js
+++ note-script.js
@@ -4,21 +4,18 @@ const fs = require("fs");
 const {Command} = require("commander");
 
 function makeDir(dirName) {
-    fs.mkdir(dirName, {recursive:true}, (err) => console.error(err))
-    
+    fs.mkdir(`${dirName}-notes`, {recursive:true}, (err) => console.error(err))
 }
 
 function createFiles(filePath) {
-
     if (sessions) {
         for (let i = 1; i <= sessions; i++) {
-            fs.writeFileSync(`${filePath}/${filePath}-stream${i}-notes.md`,noteTemplate);
-            fs.writeFileSync(`${filePath}/${filePath}}-stream${i}-notes.md`,additionalNoteTemplate)
-            
+            fs.writeFileSync(`${filePath}-notes/${filePath}-stream${i}-notes.md`,noteTemplate);
+            fs.writeFileSync(`${filePath}-notes/${filePath}-stream${i}-additional-notes.md`,additionalNoteTemplate)
         }
     } else {
-        fs.writeFileSync(`${filePath}/${filePath}-notes.md`,noteTemplate);
-        fs.writeFileSync(`${filePath}/${filePath}-additional-notes.md`, additionalNoteTemplate);
+        fs.writeFileSync(`${filePath}-notes/${filePath}-notes.md`,noteTemplate);
+        fs.writeFileSync(`${filePath}-notes/${filePath}-additional-notes.md`, additionalNoteTemplate);
     }
 }
 
@@ -27,7 +24,7 @@ const program = new Command();
 program
     .requiredOption('-d, --date <date>', 'date of the session')
     .option('-s, -sessions <sessions>', 'the quantity of sessions to generate')
- 
+
 program.parse(process.argv)
 
 const options = program.opts();
@@ -41,3 +38,4 @@ let sessions = options.Sessions;
 makeDir(date)
 createFiles(date)
 
+console.log(sessions ? `🎉 ${ sessions } sets of notes for ${date} generated successfully!` : `🎉 notes files for ${date} generated successfully!`)
